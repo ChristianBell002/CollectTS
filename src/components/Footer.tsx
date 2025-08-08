57d0e7f2bb0299e5b1154797e8810448a4b61d01
Christian Bello <christianbello416@gmail.com>
2025-08-08 14:07:18 -0500

    Small changes: Update footer text and improve styling in index.css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@
 export const Footer = () => {
     return (
         <div className = "footer">
-            <p>&copy; 2025 ViewCollect. All rights reserved. </p>
+            <p>&copy; 2025 ViewCollect. All rights reserved. Follow us on social media!</p>
             
-            <p>Follow us on social media!</p>
+            {/* <p>Follow us on social media!</p> */}
             <ul className="social-media-links"/>
         </div>
     )
